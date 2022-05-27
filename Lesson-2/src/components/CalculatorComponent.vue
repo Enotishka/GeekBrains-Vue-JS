81d87c52af516d3f3a25c1686a093593e6278134
Enotishka <enotishka18@gmail.com>
2022-05-27 16:30:43 +0300

    Lesson-2 solution

--- Lesson-2/src/components/CalculatorComponent.vue
+++ Lesson-2/src/components/CalculatorComponent.vue
@@ -1,21 +1,41 @@
 <template>
 <div>
   <div class="main">
-    <label for="operand1">
+      <!-- eslint-disable-next-line -->
       <input id="operand1" type="number" v-model.number="operand1">
-    </label>
-    <label for="operand2">
+      <!-- eslint-disable-next-line -->
       <input id="operand2" type="number" v-model.number="operand2">
-    </label>
     = {{ result }}
   </div>
-  <div>
+  <div class="operators">
     <button @click="result = operand1 + operand2">+</button>
     <button @click="sub">-</button>
     <button @click="div">/</button>
     <button @click="mult">*</button>
     <button @click="pow">**</button>
     <button @click="integer">/</button>
+  </div>
+  <div>
+    <label for="checkbox">
+      <input type="checkbox" name="checkbox" v-model="checked">
+      Отобразить экранную клавиатуру
+    </label>
+  </div>
+  <div v-if="checked">
+    <div class="keyboard" >
+      <button v-for="num in 10" @click="keyboard(num - 1)" :key="num">{{num - 1}}</button>
+      <button @click="back">🠔</button>
+    </div>
+    <div>
+      <label for="one">
+        <input type="radio" id="one" value="Один" v-model="picked">
+        Операнд1
+      </label>
+      <label for="two">
+        <input type="radio" id="two" value="Два" v-model="picked">
+        Операнд2
+      </label>
+    </div>
   </div>
 </div>
 </template>
@@ -28,6 +48,8 @@ export default {
       operand1: 0,
       operand2: 0,
       result: 0,
+      checked: false,
+      picked: 'Один',
     };
   },
   methods: {
@@ -46,6 +68,27 @@ export default {
     integer() {
       this.result = parseInt(this.operand1 / this.operand2, 10);
     },
+    keyboard(num) {
+      if (this.picked === 'Один') {
+        this.operand1 = this.operand1 * 10 + num;
+      } else {
+        this.operand2 = this.operand2 * 10 + num;
+      }
+    },
+    back() {
+      if (this.picked === 'Один') {
+        this.operand1 = parseInt(this.operand1 / 10, 10);
+      } else {
+        this.operand2 = parseInt(this.operand2 / 10, 10);
+      }
+    },
   },
 };
+
 </script>
+
+<style scoped>
+.operators {
+  margin-bottom: 50px;
+}
+</style>
